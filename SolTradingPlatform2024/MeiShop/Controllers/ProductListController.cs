c3eeaa288223d2f550440a546b44fbe0b2c7e25f
Emmanuel <51800905@edu.campus02.at>
2024-05-25 09:49:58 +0200

    Initial commit
    Base project

---
+++ SolTradingPlatform2024/MeiShop/Controllers/ProductListController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeiShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductListController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Windows Phone", "BlackBerry", "iPhone" };
+        }
+    }
+}
