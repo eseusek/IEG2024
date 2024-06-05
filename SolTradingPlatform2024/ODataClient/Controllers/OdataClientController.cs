b3ec82d51f3e474ca068a036236c9cbacbcbc25c
Emmanuel <51800905@edu.campus02.at>
2024-06-05 17:28:12 +0200

    Aufgabe8Erledigt

---
+++ SolTradingPlatform2024/ODataClient/Controllers/OdataClientController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ODataService.Models;
+using System;
+
+namespace ODataClient.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OdataClientController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            var serviceRoot = "http://localhost:5139/odata/";
+            var context = new Default.Container(new Uri(serviceRoot));
+
+            return await context.Product.ExecuteAsync();            
+        }
+    }
+}
---
+++ SolTradingPlatform2024/ODataService/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿namespace ODataService.Controllers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using ODataService.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.OData.Query;
+    using Microsoft.AspNetCore.OData.Routing.Controllers;
+
+    public class ProductController : ODataController
+    {
+        private static List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "BlackBerry", Price = 100.0m },
+            new Product { Id = 2, Name = "Windows Phone", Price = 300.0m },
+            new Product { Id = 3, Name = "iPhone", Price = 900.0m }
+        };
+
+        [EnableQuery]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            return Ok(products);
+        }
+
+        [EnableQuery]
+        public ActionResult<Product> Get([FromRoute] int key)
+        {
+            var item = products.SingleOrDefault(d => d.Id.Equals(key));
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+    }
+}
